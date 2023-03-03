8f5582beac5352dddd43c9dec1fe9dba9bdfa728
gtfobar <bargtfo@gmail.com>
2023-03-03 23:09:11 +0300

    Enhanced logging. Fixed minor bugs.

--- rustore-apk-scanner.py
+++ rustore-apk-scanner.py
@@ -15,10 +15,11 @@ DOWNLOAD_LINK_URL = 'https://backapi.rustore.ru/applicationData/download-link'
 POST_DATA_TEMPLATE = {"appId":5364415,"firstInstall":True}
 APPS_NUMBER_LIMIT = 5
 USER_TOKEN = 'vk1.a.86BdzHbpnjTmgDToW22JBsF82MIj_hFab5n1lId2P_50NeFzrwE6yA5HzRqsNTDxI5ToP0ADRZdygy1Ou2ses73rvbFDpXYgLqjf38Aebv3ks5ba27s5QpnX__AWvOG9bID_vuN-inGdfP4nhh9soJ4PsdepY-_21PaKMEWUfJQC0O0-wkpkgm-Ihm5y5-NXXfJ5bej279SlFhRnqjLpXA'
-INIT_PAGE_SIZE = 1000
+INIT_PAGE_SIZE = 2000
 SMS_CONSENT_FINGERPRINT = 'EXTRA_CONSENT_INTENT'.encode('utf-8')
+LOG_FILE = 'rustore-apk-scanner.log'
 
-logging.basicConfig(filename='rustore-apk-scanner.log', format='%(levelname)s:%(message)s', level=logging.INFO)
+logging.basicConfig(filename=LOG_FILE, format='%(levelname)s:%(message)s', level=logging.INFO)
 
 def get_categories_json(local_copy = False):
     categories_json = requests.get(CATEGORIES_URL).json()
@@ -31,6 +32,7 @@ def get_categories_from_store():
     logging.info('Getting categories from store...')
     categories_json = get_categories_json()
     categories = [category['category'] for category in categories_json['body']['content']]
+    logging.info(f'Got {len(categories)} categories.')
     return categories
 
 def get_categories_from_local(path):
@@ -39,12 +41,14 @@ def get_categories_from_local(path):
     return categories
 
 def get_apps_from_category(category, local_copy=False):
+    logging.info(f'Extracting apps from category {category}...')
     apps_json = requests.get(APP_INFO_URL_TEMPLATE.format(
         category=category,
         page_size=INIT_PAGE_SIZE)).json()['body']['content']
     if (local_copy is True):
         with open(BASE_DIR + JSON_PATH + category + '.json', 'w') as file:
             json.dump(apps_json, file)
+    logging.info(f'Extracted {len(apps_json)} apps.')
     return apps_json
 
 def get_apps():
@@ -69,43 +73,51 @@ def get_apk_url(appId, token=USER_TOKEN):
 
 def download_apk(appId, path):
     url = get_apk_url(appId)
-    logging.info(f'Downloading apk of {appId}')
+    logging.info(f'Downloading apk of {appId}...')
     with urllib.request.urlopen(url) as response, open(path, 'wb') as out_file:
         shutil.copyfileobj(response, out_file)
 
 def decompile(in_apk, out_dir):
-    logging.info(f'Decompiling {in_apk} to {out_dir}')
-    os.system(f'if [ ! -d {out_dir} ]; then unzip -u {in_apk} -d {out_dir} > /dev/null; fi')
+    logging.info(f'Decompiling {in_apk} to {out_dir}...')
+    if (not os.path.exists(out_dir)):
+        os.system(f'unzip -u {in_apk} -d {out_dir} > /dev/null')
 
 def grep(path):
     # os.system(f' for f in $(find {path} -type f -name "*.dex"); do strings $f | grep EXTRA_CONSENT_INTENT; done}')
     file_extension_re = re.compile('.*\.dex')
-    logging.info(f'Looking for sms consent library usage in {path}')
+    logging.info(f'Looking for sms consent library usage in {path}...')
     for root, dirs, fnames in os.walk(path):
         for fname in fnames:
             if (not file_extension_re.match(fname)):
                 continue
             with open(os.path.join(root, fname), 'rb') as f:
                 if (f.read().find(SMS_CONSENT_FINGERPRINT) >= 0):
-                    logging.info('Sms consent library usage detected')
+                    logging.info(f'Sms consent library usage detected in {fname}.')
                     return True
+    return False
 
 def uses_sms_consent_api(apk_path):
     decompiled_dir = f'{apk_path[:-4]}_decompiled'
     decompile(apk_path, decompiled_dir)
-    grep(decompiled_dir)
+    result = grep(decompiled_dir)
     shutil.rmtree(decompiled_dir)
+    logging.info(f'Search finished. Deleting {decompiled_dir}...')
+    return result
 
 def main():
+    print(f'Logging into {LOG_FILE}')
     logging.info("Starting...")
     apps = get_apps()
+    if (not os.path.exists(APK_BASE_DIR)):
+        os.mkdir(APK_BASE_DIR)
     for app in apps:
         packageName = app["packageName"]
         appId = app['appId']
         logging.info(f'{packageName} has appId={appId}')
         apk_path = os.path.join(APK_BASE_DIR, f'{packageName}.apk')
         download_apk(app['appId'], apk_path)
-        if (not uses_sms_consent_api(apk_path)):
+        if (not uses_sms_consent_api(apk_path) is True):
+            logging.info('Sms consent library not found. Deleting apk...')
             os.remove(apk_path)
 
 main()
